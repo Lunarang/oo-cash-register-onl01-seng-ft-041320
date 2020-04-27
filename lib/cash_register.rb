557b78964b52f6e72a0fb4b6e5f5ba822248655d
Erin Mettille <ecmettille@gmail.com>
2020-04-27 00:58:19 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,9 +1,10 @@
 class CashRegister 
-  
+  attr_accessor :total, :discount, :cart
+ 
   def initialize (discount = 0)
     @total = 0 
     @discount = discount
-    
+    @cart = []
   end
   
   
